18a06eeb8088b363e994d0d75dbf8c169fc1a2da
mapdio <77219481+mapdio@users.noreply.github.com>
2021-02-09 19:12:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,7 +31,7 @@ sed -i '$a src-git small  https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git OpenAppFilter https://github.com/OpenWrt-Actions/OpenAppFilter' feeds.conf.default
 
 # URL替换
-sed -i s'/https:＼/＼/downloads.pangubox.com/http:＼/＼/downloads.pangubox.com:6380/' scripts/download.pl
+sed -i s'/https:\/\/downloads.pangubox.com/http:\/\/downloads.pangubox.com:6380/' scripts/download.pl
 
 # 仅编译R7800固件
 sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += netgear_r7800|TARGET_DEVICES += netgear_r7800|' target/linux/qualcomm/image/ipq806x.mk
